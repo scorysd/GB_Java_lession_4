7e1cb5f7967ea448201147ec717b7430b5030e95
Roman <toolatet@gmail.com>
2022-09-23 16:16:39 +0700

    TODO

--- Task4.java
+++ Task4.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 
 /**
  * Task4
@@ -5,32 +6,67 @@
 public class Task4 {
     public static int[][] createArr(int a, int b) {
         int[][] matrix = new int[a][b];
-        return  matrix;
+        return matrix;
     }
     public static void printArr(int matrix[][]) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int  j= 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]+" ");
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
-        System.out.println();
+            System.out.println();
         }
     }
-    public static void findPath(int matrix[][], int x, int y) {
-        
-        if (matrix[x][y]==0) {
-            matrix[x][y]=1;
-            findPath(matrix, x-1, y-1);
-        } else {
-            
+    public static void FillArr(int matrix[][]) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 8;
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 8;
+                for (int k = 0; k < matrix.length; k++) {
+                    matrix[k][matrix[0].length - 1] = 8;
+                    for (int l = 0; l < matrix.length; l++) {
+                        matrix[matrix.length - 1][l] = 8;
+                    }
+                }
+            }
         }
     }
-    
+    public static void findPath(int matrix[][], int x, int y, int step) {
+        ArrayDeque<Integer> steps = new ArrayDeque<>();
+        if (matrix[x - 1][y] == 0 && x > 1) {
+            matrix[x - 1][y] = step;
+            steps.addLast(x - 1);
+            steps.addLast(y);
+            step++;
+            findPath(matrix, steps.poll(), steps.poll(), step);
+        }
+        if (matrix[x][y + 1] == 0 && y < 9) {
+            matrix[x][y + 1] = step;
+            steps.addLast(x);
+            steps.addLast(y + 1);
+            step++;
+            findPath(matrix, steps.poll(), steps.poll(), step);
+        }
+        if (matrix[x + 1][y] == 0 && x < 9) {
+            matrix[x + 1][y] = step;
+            steps.addLast(x + 1);
+            steps.addLast(y);
+            step++;
+            findPath(matrix, steps.poll(), steps.poll(), step);
+        }
+        if (matrix[x][y - 1] == 0 && y > 1) {
+            matrix[x][y - 1] = step;
+            steps.addLast(x);
+            steps.addLast(y - 1);
+            findPath(matrix, steps.poll(), steps.poll(), step);
+        }
+    }
+
     public static void main(String[] args) {
-        // printArr(createArr(10,10));
+        printArr(createArr(10, 10));
         int[][] matrix = createArr(10, 10);
-        matrix[3][4] = 1;
-        matrix[7][9] = 99;
+        FillArr(matrix);
+        findPath(matrix, 5, 4, 1);
+        System.out.println("_________");
         printArr(matrix);
-        findPath(matrix, 3, 4);
     }
 }
